7dc479360f86b830a183c9ba9d5656f0f8b27837
Andri Krämer <akraemer@matlon.ch>
2024-11-16 21:10:39 +0100

    introduced project categories

--- matlon/_modidx.py
+++ matlon/_modidx.py
@@ -8,6 +8,8 @@ d = { 'settings': { 'branch': 'main',
   'syms': { 'matlon.model': { 'matlon.model.Project': ('model.html#project', 'matlon/model.py'),
                               'matlon.model.Project.__eq__': ('model.html#project.__eq__', 'matlon/model.py'),
                               'matlon.model.Project.short_title': ('model.html#project.short_title', 'matlon/model.py'),
+                              'matlon.model.filter_projects': ('model.html#filter_projects', 'matlon/model.py'),
+                              'matlon.model.get_categories': ('model.html#get_categories', 'matlon/model.py'),
                               'matlon.model.load_projects': ('model.html#load_projects', 'matlon/model.py'),
                               'matlon.model.project_to_markdown': ('model.html#project_to_markdown', 'matlon/model.py'),
                               'matlon.model.projects_to_dot': ('model.html#projects_to_dot', 'matlon/model.py')}}}
